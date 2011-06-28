a693b1ff9ef7f16e00b82df73190f17eaf327d3b
Steven Gravell <steve.gravell@smarkets.com>
2011-06-28 15:07:25 +0100

    fix when included as dependancy

--- piqic.sh
+++ piqic.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
-(cd deps/smk_api_common/ \
+SMK_API_COMMON=deps/smk_api_common/
+if [ ! -d "$SMK_API_COMMON" ]; then
+  SMK_API_COMMON=../smk_api_common/
+fi
+
+API_HOME="$PWD"
+(cd "$SMK_API_COMMON" \
   && piqic erlang -I ../eto_common/ seto.piqi \
-  && mkdir -p ../../{src,include} \
-  && mv *.hrl ../../include/ \
-  && mv *.erl ../../src/ \
+  && mkdir -p "$API_HOME"/{src,include} \
+  && mv *.hrl "$API_HOME"/include/ \
+  && mv *.erl "$API_HOME"/src/ \
 )
